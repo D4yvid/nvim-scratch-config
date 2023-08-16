6fa5362cd2f1b3c18ca8c876f85c1d8aa4531a1c
Dayvid Albuquerque <kkd4yv1d@gmail.com>
2023-08-16 19:04:32 -0300

    bro i only wrote 5 lines in the phone :skull:

--- lua/lsp/buffer.lua
+++ lua/lsp/buffer.lua
@@ -1,11 +1,25 @@
 local M = {}
 
 function M:new(buffer, client)
-	return {
-		buffer = buffer,
-		client = client,
-		requests = {}
-	}
+	local self = setmetatable({}, { __index = M })
+
+	self.buffer = buffer
+	self.client = client
+	self.requests = {}
+
+	return self
+end
+
+function M:request_completion(callback)
+	-- Inspired in cmp_nvim_lsp
+
+	if self.requests.completion ~= nil then
+		self.client.cancel_request(self.requests.completion)
+
+		self.requests.completion = nil
+	end
+
+	local _, req_id = self:run_request()
 end
 
 return M
